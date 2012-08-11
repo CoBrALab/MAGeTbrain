cb41178007b6520ee37aca6daae33b70715e0ab3
Jon Pipitone <jon.pipitone@utoronto.ca>
2012-08-11 13:24:26 -0400

    Updates to voting to fix target label bug

--- bin/vote.py
+++ bin/vote.py
@@ -6,6 +6,7 @@ import shutil
 import logging
 import os
 import os.path
+from os.path import join as joinp
 import glob
 import subprocess
 import errno
@@ -56,20 +57,21 @@ def resample_labels(atlas, template, target, labels_dir, registration_dir, outpu
         (nlxfm, target.image, invert, template_labels, target_labels)
     return (target_labels, cmd)      
  
-def parallel(commands, processors = 8):
+def parallel(commands, processors = 8, dry_run = False):
     "Runs the list of commands through parallel"
     command = 'parallel -j%i' % processors
-    execute(command, input='\n'.join(commands))
+    execute(command, input='\n'.join(commands), dry_run = dry_run)
 
-def execute(command, input = ""):
+def execute(command, input = "", dry_run = False):
     """Spins off a subprocess to run the cgiven command"""
     logger.debug("Running: " + command + " on:\n" + input)
-    
-    proc = subprocess.Popen(command.split(), 
-                            stdin = subprocess.PIPE, stdout = 2, stderr = 2)
-    proc.communicate(input)
-    if proc.returncode != 0: 
-        raise Exception("Returns %i :: %s" %( proc.returncode, command ))
+   
+    if not dry_run:  
+        proc = subprocess.Popen(command.split(), 
+                                stdin = subprocess.PIPE, stdout = 2, stderr = 2)
+        proc.communicate(input)
+        if proc.returncode != 0: 
+            raise Exception("Returns %i :: %s" %( proc.returncode, command ))
     
 def mkdirp(*p):
     """Like mkdir -p"""
@@ -93,47 +95,19 @@ def compare_similarity(image_path, expected_labels_path, computed_labels_path, o
         "validation_v%i.csv" % validation, [expected_labels_path, computed_labels_path])
     return (validation_output_file, cmd)
         
-def majority_vote(subject, labels, output_dir):
-    cmd, voted_labels = command("vote_majority.py", output_dir, "labels.mnc", labels)
-    return (voted_labels, cmd)
+def do_vote(voting_templates, target_vote_dir, temp_labels_dir):
+    """Helper function for vote() """
+    resample_cmds = []
+    target_labels =  []
+    for atlas in atlases:
+        for template in voting_templates:
+            labels, cmd = resample_labels(atlas, template, target, template_labels_dir, registrations_dir, temp_labels_dir, inverse=False)
+            resample_cmds.append(cmd)
+            target_labels.append(labels)
 
-def weighted_vote(labels_dir, vote_dir, scores, target, templates, atlases, top_n):
-        """Produces a command that does a weighted voting for the given target
-
-           labels_dir - the directory containing the atlas/template/target/labels.mnc files
-           score_dir  - the directory containing the template/target/<score>.txt files
-           score_filename - the actually name of the score file (xcorr.txt, nmi.txt, etc..)
-           target     - the target image (Template instance)
-           atlases    - the atlases (as Template instances) to consider labels from 
-           templates  - templates (as Template instances) to consider labels from
-           top_n      - the number of templates to consider labels from when voting
-
-           Returns the path to the voted on labels, along with the command to run.
-        """
-
-        # STEP 2: select the top_n templates, and all of the labels for them from the atlases
-        labels_list = []
-        sorted_templates = sorted(templates, key= lambda x:scores.get((x.stem,target.stem),0), reverse=True)
-        for template in sorted_templates[:top_n]:
-            labels_list.extend([os.path.join(labels_dir, atlas.stem, template.stem, target.stem, "labels.mnc") for atlas in atlases])
-            
-        # STEP 3: majority vote!
-        cmd, weighted_voted_labels = command("vote_majority.py", vote_dir, "labels.mnc", labels_list)
-        return (weighted_voted_labels, cmd)
-
-def xcorr_vote(labels_dir, vote_dir, target, templates, atlases, top_n):
-    return weighted_vote(labels_dir, vote_dir, xcorr_scores, target, templates, atlases, top_n)
+    vote_cmd, labels = command("voxel_vote.py", target_vote_dir, "labels.mnc", target_labels)
+    return (vote_cmd, resample_cmds)
     
-def nmi_vote(labels_dir, vote_dir, target, templates, atlases, top_n):
-    return weighted_vote(labels_dir, vote_dir, nmi_scores, target, templates, atlases, top_n)
-
-def mk_dir(dirlist, *p):
-    dir = os.path.join(*p)
-    dirlist.append(dir)
-    return dir
-
-def consolidate_results_cmd(files, label_ids, output_file):
-    return "grep '^%s,' %s > %s" % (',\|'.join(map(str,label_ids)), ' '.join(files), output_file)
    
 def vote(target):
     """Generate the commands to vote on this target image.
@@ -141,9 +115,7 @@ def vote(target):
        This "function" relies on lots of stuff being in module scope, specifically: 
         - atlases
         - templates
-        - temp_labels_dir
         - registrations_dir
-        - temp_labels_dir
         - fusion_dir
         - score_dir
         - options
@@ -158,64 +130,38 @@ def vote(target):
     resample_cmds = []
     voting_cmds = []
 
-    ####################################################
-    # Resample labels
-    #################################################### 
-    target_labels = {}
-    for atlas in atlases:
-        l = []
-        for template in templates:
-            labels, cmd = resample_labels(atlas, template, target, template_labels_dir, registrations_dir, temp_labels_dir, inverse=False)
-            resample_cmds.append(cmd)
-            l.append(labels)
-        target_labels[target] = l 
-    
-    ####################################################
-    # VOTE 
-    #################################################### 
-    
-    # majority voting fusion
-    #################################################### 
     if options.majvote:
-        target_vote_dir = mkdirp(fusion_dir, "majority_vote", target.stem)
-        labels, vote_cmd = majority_vote(target.image, target_labels[target], target_vote_dir)
-        voting_cmds.append(vote_cmd)
+        target_vote_dir = mkdirp(fusion_dir, "majvote", target.stem)
+        if not os.path.exists(joinp(target_vote_dir, 'labels.mnc')):
+            vote_cmd, resamples = do_vote(templates, target_vote_dir, temp_labels_dir)
+            voting_cmds.append(vote_cmd)
+            resample_cmds.extend(resamples)
 
     if options.xcorr:
-        top_n_templates = options.xcorr
-        top_n_labels = top_n_templates * len(atlases)  # one label for each atlas per template
-        logger.info("XCorr weighted voting with %i templates", top_n_templates)
-        xcorr_dir = mkdirp(fusion_dir, "xcorr")
-        xcorr_vote_dir = mkdirp(xcorr_dir, target.stem)
-        
-        xcorr_vote_labels, vote_cmd = xcorr_vote(temp_labels_dir, xcorr_vote_dir, target, templates, atlases, top_n_labels) 
-        voting_cmds.append(vote_cmd)
+        target_vote_dir = mkdirp(fusion_dir, "xcorr", target.stem)
+        if not os.path.exists(joinp(target_vote_dir, 'labels.mnc')):
+            top_n            = options.xcorr
+            scores           = xcorr_scores
+            sorted_templates = sorted(templates, key= lambda x:scores.get((x.stem,target.stem),0), reverse=True)
+            vote_cmd, resamples = do_vote(sorted_templates[:top_n], target_vote_dir, temp_labels_dir)
+            voting_cmds.append(vote_cmd)
+            resample_cmds.extend(resamples)
 
     if options.nmi:
-        top_n_templates = options.nmi
-        top_n_labels = top_n_templates * len(atlases)  # one label for each atlas per template
-        logger.info("NMI weighted voting with %i templates", top_n_templates)
-        nmi_dir = mkdirp(fusion_dir, "nmi")
-        nmi_vote_dir = mkdirp(nmi_dir, target.stem)
+        target_vote_dir = mkdirp(fusion_dir, "nmi", target.stem)
+        if not os.path.exists(joinp(target_vote_dir, 'labels.mnc')):
+            top_n            = options.nmi
+            scores           = nmi_scores
+            sorted_templates = sorted(templates, key= lambda x:scores.get((x.stem,target.stem),0), reverse=True)
+            vote_cmd, resamples = do_vote(sorted_templates[:top_n], target_vote_dir, temp_labels_dir)
+            voting_cmds.append(vote_cmd)
+            resample_cmds.extend(resamples)
 
-        nmi_vote_labels, vote_cmd = nmi_vote(temp_labels_dir, score_dir, nmi_vote_dir, target, templates, atlases, top_n_labels) 
-        voting_cmds.append(vote_cmd)
-
-    ####################################################
-    # Run commands
-    #################################################### 
-    
-    # sort the resampling commands to help with disk caching (maybe?)
-    def extract_xfm(cmd):
-        return re.search(r'\S*.xfm',cmd).group(0)
-    resample_cmds = list(set(resample_cmds))
-    resample_cmds.sort(lambda x,y: cmp(extract_xfm(x), extract_xfm(y))) 
-    
     logger.info("Resampling labels ...")
-    parallel(resample_cmds, options.processes)
+    parallel(set(resample_cmds), options.processes, options.dry_run)
 
     logger.info("Voting...")
-    parallel(set(voting_cmds), options.processes)
+    parallel(set(voting_cmds), options.processes, options.dry_run)
 
     logger.info("Cleaning up...")
     shutil.rmtree(temp_dir)
@@ -241,6 +187,10 @@ if __name__ == "__main__":
     parser.add_option("--output_dir", dest="output_dir",
         default="output", type="string", 
         help="Path to output folder")
+    parser.add_option("-n", dest="dry_run",
+        default=False,
+        action="store_true", 
+        help="Short string to use to uniquely identify the results file (default: date & time)")
     options, args = parser.parse_args()
 
     output_dir        = os.path.abspath(options.output_dir)
@@ -249,10 +199,9 @@ if __name__ == "__main__":
     
     ## Set up TEMP space
     persistent_temp_dir   = tempfile.mkdtemp(dir='/dev/shm/')
-    execute("tar xzf output/labels.tar.gz -C " + persistent_temp_dir)
+    execute("tar xzf output/labels.tar.gz -C " + persistent_temp_dir, dry_run = options.dry_run)
     xcorr_scores = read_scores(os.path.join(output_dir, "xcorr.csv"))
     template_labels_dir = mkdirp(persistent_temp_dir, "labels")    
-    score_dir = os.path.join(persistent_temp_dir, "scores") 
 
     # 
     atlases   = get_templates('input/atlases')
--- bin/voxel_vote.py
+++ bin/voxel_vote.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
+import logging
+FORMAT = '%(asctime)-15s %(name)s %(levelname)s: %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
+logger.debug("Starting imports...")
+import subprocess
 from pyminc.volumes.factory import *
 from numpy import *
 from scipy.stats import *
 from optparse import OptionParser
-import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('voxel_vote.py')
+logger.debug("Done imports.  Starting program")
+
+def execute(command, input = ""):
+    """Spins off a subprocess to run the cgiven command"""
+    logger.debug("Running: " + command + " on:\n" + input)
+    
+    proc = subprocess.Popen(command.split(), 
+                            stdin = subprocess.PIPE, stdout = 2, stderr = 2)
+    proc.communicate(input)
+    if proc.returncode != 0: 
+        raise Exception("Returns %i :: %s" %( proc.returncode, command ))
 
 if __name__ == "__main__":
 
@@ -19,8 +33,9 @@ if __name__ == "__main__":
                       help="clobber output file",
                       type="string")
 
+    logger.debug("parsing arguments.")
     (options, args) = parser.parse_args()
-
+    
     if len(args) < 3:
         parser.error("Incorrect number of arguments")
 
@@ -31,13 +46,11 @@ if __name__ == "__main__":
     
     volhandles = []
 
+    logger.debug("loading input volumes....")
     nfiles = len(args)-1
-    try:
-        for i in range( nfiles ):
-            volhandles.append(volumeFromFile(args[i], dtype='ubyte'))
-    except Exception, e:
-        logger.exception("Error whilst reading file %s", args[i])
-        raise e
+    for i in range( nfiles ):
+        volhandles.append(volumeFromFile(args[i], dtype='ubyte'))
+    logger.debug("loading input volumes complete.")
 
     outfile = volumeFromInstance(volhandles[0], outfilename)
 #    outdist = volumeFromInstance(volhandles[0], outfiledist)
@@ -46,6 +59,7 @@ if __name__ == "__main__":
                          volhandles[0].sizes[1],
                          volhandles[0].sizes[2]))
                          
+    logger.debug("computing slice votes...")
     for i in range(volhandles[0].sizes[0]):
         for j in range(nfiles):
             t = volhandles[j].getHyperslab((i,0,0),
@@ -57,8 +71,15 @@ if __name__ == "__main__":
         outfile.data[i::] = mode(sliceArray)[0]
         #outdist.data[i::] = mode(sliceArray)[1]/nfiles
 
+    logger.debug("writing output file...")
     outfile.writeFile()
     outfile.closeVolume()
+    
+    execute('mincconvert %s %s.v1' % (outfilename, outfilename))
+    execute('mincconvert -2 -clob -compress 9 %s.v1 %s' % (outfilename, outfilename))
+    execute('rm  %s.v1' % outfilename)
+
+    logger.debug("writing output file complete.")
     #outdist.writeFile()
     # outdist.closeVolume()
 
