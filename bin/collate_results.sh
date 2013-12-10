f2a5d43dedabd727eded3d8c8300722dfeb31c89
Jon Pipitone <jon@utoronto.ca>
2013-12-09 20:30:50 -0500

    collaged results now include volumes$

--- bin/collate_results.sh
+++ bin/collate_results.sh
@@ -5,12 +5,40 @@
 # This script makes a number of assumptions, but you'll have to read the code
 # to figure that bit out. Sorry.
 #
-out=results.csv
 (
-echo "batch,timestamp,approach,method,atlases,templates,top_n,subject,label,k,se,sn,j"
+echo "batch,file,label,k,se,sn,j"
 for batch in {1..10}; do
   cat output/fusion_${batch}/*_similarity.csv | sed \
-    -e "s#^#batch_${batch},#g" \
+    -e "s#^#batch_${batch},#g" 
+done
+) > raw_sim.csv
+
+(
+echo batch,$(grep -h file output/fusion_*/*_volumes.csv | sort | uniq)
+for batch in {1..10}; do
+  cat output/fusion_${batch}/*_volumes.csv | \
+    grep -v file | \
+    sed -e "s#^#batch_${batch},#g" 
+done
+) > raw_vol.csv
+
+R --no-save <<'RCODE'
+library(reshape)
+s = read.csv('raw_sim.csv')
+v = read.csv('raw_vol.csv', check.names=FALSE) # allow numeric names
+
+melted = melt(v, id.vars=c('batch','file'))
+colnames(melted) = c('batch', 'file', 'label', 'volumes')
+joined = merge(s, melted, by = c('batch', 'file', 'label'))
+
+write.csv(joined, 'raw_joined.csv')
+RCODE
+
+
+(
+echo "batch,timestamp,approach,method,atlases,templates,top_n,subject,label,k,se,sn,j,volume"
+tail -n+2 raw_joined.csv | cut -f2- -d, | sed \
+    -e 's#"##g' \
     -e 's#_templates/#_templates_0_topn/#g' \
     -e 's#/dev/shm/##g' \
     -e 's#/labels.mnc##g' \
@@ -19,6 +47,6 @@ for batch in {1..10}; do
     -e 's#_topn/.*/#/#g' \
     -e 's#/#,#g' \
     -e 's#,multiatlas,#,ma,#g' 
-done
-) > $out
+) > results.csv
 
+rm raw_vol.csv raw_sim.csv raw_joined.csv
