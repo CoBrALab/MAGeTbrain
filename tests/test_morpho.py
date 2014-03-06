37078376240edb8a88536d89a594973ea585fe03
Jon Pipitone <jon.pipitone@gmail.com>
2014-03-05 20:21:23 -0500

    Updated tests

--- tests/test_morpho.py
+++ tests/test_morpho.py
@@ -20,13 +20,19 @@ class TestMorphobits(unittest.TestCase):
   def test_model_only_single_subject_sanity_checks(self):
     tasklist  = self.morpho.build_pipeline(*self.model_only_1_subject)
 
-    for stage_name in [ 'subject.nuc', 'subject.to.modelspace',
-        'subject.to.modelspace', 'pairwise.reg', 'model.subject.xfm',
-        'model.subject.gridavg', 'model.subject.displace']:
+    for stage_name, count in {'image.prep'            : 2,  # autocrop and nuc
+                              'subject.to.modelspace' : 1,
+                              'subject.to.modelspace' : 1, 
+                              'pairwise.reg'          : 1, 
+                              'model.subject.xfm'     : 2,  # nl and nl-only xfms
+                              'model.subject.gridavg' : 1 , 
+                              'model.subject.displace': 1}.iteritems():
 
       stage = tasklist.stages[stage_name]
       self.assertTrue(stage, msg=stage_name + ' - ' + repr(stage))
-      self.assertEquals(len(stage), 1, msg=stage_name + ' - ' + repr(stage))
+      self.assertEquals(len(stage), count, 
+          msg="Stage {} has {} commands, but only {} expected: {}".format(
+            stage_name, len(stage), count, "\n".join(map(str,stage))))
 
 if __name__ == '__main__':
   unittest.main()
