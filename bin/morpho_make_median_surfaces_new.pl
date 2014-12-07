843c00a8a4d2e98763ecf04ac9b43aaea36f89c5
raihaan <patelraihaan@gmail.com>
2014-12-07 18:18:01 -0500

    now references scripts in /home/m/mchakrav/patlemo6/Median_code

--- bin/morpho_make_median_surfaces_new.pl
+++ bin/morpho_make_median_surfaces_new.pl
@@ -104,7 +104,7 @@ my $R_command_points = "R --slave --no-save --silent ".
   "${tmpdir}/median_xcoords.csv ".
   "${tmpdir}/median_ycoords.csv ".
   "${tmpdir}/median_zcoords.csv ".
-  "< $PWD/barf_medians_driver.R ";
+  "< /home/m/mchakrav/patelmo6/Median_code/barf_medians_driver.R ";
 do_cmd($R_command_points);
   #"< /projects/mallar/NIH_COS_2/Median_surface_code/barf_medians_driver.R ";
 
@@ -115,7 +115,7 @@ my $R_command_normals = "R --slave --no-save --silent ".
   "${tmpdir}/median_xnorms.csv ".
   "${tmpdir}/median_ynorms.csv ".
   "${tmpdir}/median_znorms.csv ".
-  "< $PWD/barf_medians_driver.R ";
+  "< /home/m/mchakrav/patelmo6/Median_code/barf_medians_driver.R ";
 do_cmd($R_command_normals);
   #"< /projects/mallar/NIH_COS_2/Median_surface_code/barf_medians_driver.R ";
 
