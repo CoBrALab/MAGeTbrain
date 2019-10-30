8a7419a74c83c99ad7b1016596e7b5fa8f61030e
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-10-30 14:59:37 -0400

    Update to allants registration command

--- bin/mb_generate_iterations_singlestep_affine_resscale.py
+++ bin/mb_generate_iterations_singlestep_affine_resscale.py
@@ -1,66 +1,80 @@
 #!/usr/bin/env python
 
-#This file generates steps of affine registration between two images and attempts to compensate
-#For ANTs' dependency on the resolution of the file
+# This file generates steps of affine registration between two images and attempts to compensate
+# For ANTs' dependency on the resolution of the file
 
-#We do this by defining two scales to step over
-#blur_scale, which is the real-space steps in blurring we will do
-#shrink_scale, which is the subsampling scale that is 1/2 the fwhm blur scale, adjusted for file minimum resolution
+# We do this by defining two scales to step over
+# blur_scale, which is the real-space steps in blurring we will do
+# shrink_scale, which is the subsampling scale that is 1/2 the fwhm blur scale, adjusted for file minimum resolution
 
 from __future__ import division, print_function
 
 import sys
 import numpy as np
+import math
 
 if len(sys.argv) == 2:
-  resolution = float(sys.argv[1])
+    resolution = float(sys.argv[1])
 else:
-  resolution = 1.0
+    resolution = 1.0
 
 shrinks = []
 blurs = []
 iterations = []
 bins = []
-fwhm_to_sigma = 2*np.sqrt(2*np.log(2))
+fwhm_to_sigma = 2 * np.sqrt(2 * np.log(2))
 
 blur_scale = 32
 start_scale = blur_scale / 2 / resolution
 
 for shrink_scale in range(int(np.ceil(start_scale)), 0, -1):
-    shrinks.append(str(int(min(8.0/resolution,max(1.0,np.around(shrink_scale))))))
+    shrinks.append(
+        str(int(min(8.0 / resolution, max(1.0, np.around(shrink_scale))))))
     blurs.append(str(shrink_scale * 2 * resolution / fwhm_to_sigma))
-    iterations.append(str(min(2025, int(250 * 3**(shrink_scale)))))
-    bins.append(str(int(np.around((max(32, 256 / max(1, shrink_scale * resolution)))))))
+    iterations.append(str(min(2025, int(100 * 3**(shrink_scale)))))
+    bins.append(
+        str(int(np.around((max(32, 256 / max(1, shrink_scale * resolution)))))))
 
 shrinks.append("1")
 blurs.append("0")
-iterations.append("250")
+iterations.append("100")
+bins.append(str(int(np.around((max(32, 256 / max(1, 1 * resolution)))))))
 
-transforms = ["--transform Translation[0.5]",
-              "--transform Rigid[0.5]",
-              "--transform Similarity[0.25]",
-              "--transform Affine[0.125]"]
-masks =      ["--masks [NOMASK,NOMASK]",
-              "--masks [NOMASK,NOMASK]",
-              "--masks [NOMASK,NOMASK]",
-              "--masks [NOMASK,NOMASK]",]
-
+transforms = ["--transform Translation[ 0.5 ]",
+              "--transform Rigid[ 0.5 ]",
+              "--transform Similarity[ 0.25 ]",
+              "--transform Similarity[ 0.125 ]",
+              "--transform Affine[ 0.1 ]"]
+masks = ["--masks [ NOMASK,NOMASK ]",
+         "--masks [ NOMASK,NOMASK ]",
+         "--masks [ NOMASK,NOMASK ]",
+         "--masks [ ${fixedmask},${movingmask} ]",
+         "--masks [ ${fixedmask},${movingmask} ]" ]
 
 slicestart = 0
-slicesize = int(np.around(len(shrinks) / (len(transforms)) * 1.5))
+slicestartfloat = 0.0
+slicesize = int(np.ceil(len(shrinks) / (len(transforms))))
 
-for transform in transforms:
-  print(transform, end=' \\\n')
-  print("\t--metric Mattes[${{target}},${{atlas}},1,{},None]".format(bins[slicestart+slicesize]), end=' \\\n')
-  print("\t--convergence [{},1e-6,10]".format("x".join(iterations[slicestart:slicestart+slicesize])), end=' \\\n')
-  print("\t--shrink-factors {}".format("x".join(shrinks[slicestart:slicestart+slicesize])), end=' \\\n')
-  print("\t--smoothing-sigmas {}mm".format("x".join(blurs[slicestart:slicestart+slicesize])), end=' \\\n')
-  print("\t--masks [NOMASK,NOMASK]", end=' \\\n')    
-  slicestart += int(np.around(slicesize / 2))
+for i,transform in enumerate(transforms):
+    print(transform, end=' \\\n')
+    if ( (slicestart + slicesize) > (len(shrinks) - 1)):
+        print("\t--metric Mattes[ ${{target}},${{atlas}},1,{},None ]".format(bins[-1]), end=' \\\n')
+        print("\t--convergence [ {},1e-6,10 ]".format("x".join(iterations[slicestart:])), end=' \\\n')
+        print("\t--shrink-factors {}".format("x".join(shrinks[slicestart:])), end=' \\\n')
+        print("\t--smoothing-sigmas {}mm".format("x".join(blurs[slicestart:])), end=' \\\n')
+    else:
+        print("\t--metric Mattes[ ${{target}},${{atlas}},1,{},None ]".format(bins[slicestart + slicesize]), end=' \\\n')
+        print("\t--convergence [ {},1e-6,10 ]".format("x".join(iterations[slicestart:slicestart + slicesize])), end=' \\\n')
+        print("\t--shrink-factors {}".format("x".join(shrinks[slicestart:slicestart + slicesize])), end=' \\\n')
+        print("\t--smoothing-sigmas {}mm".format("x".join(blurs[slicestart:slicestart + slicesize])), end=' \\\n')
+        slicestartfloat += (len(shrinks) / (len(transforms)))
+        slicestart = int(np.ceil(slicestartfloat))
+    print("\t" + masks[i], end=' \\\n')
 
-print("--transform Affine[0.1] \\")
-print("\t--metric Mattes[${{target}},${{atlas}},1,{},None]".format(bins[-1]), end=' \\\n')
-print("\t--convergence [{},1e-6,20]".format("x".join(iterations[len(shrinks)-5:])), end=' \\\n')
-print("\t--shrink-factors {}".format("x".join(shrinks[len(shrinks)-5:])), end=' \\\n')
-print("\t--smoothing-sigmas {}mm".format("x".join(blurs[len(shrinks)-5:])), end=' \\\n')
-print("\t--masks [${fixedmask},${movingmask}]")
+
+print("--transform Affine[ 0.05 ] \\")
+print("\t--metric Mattes[ ${{target}},${{atlas}},1,{},None ]".format(bins[-1]), end=' \\\n')
+print("\t--convergence [ {},1e-7,10 ]".format("x".join(iterations[slicestart:])), end=' \\\n')
+print("\t--shrink-factors {}".format("x".join(shrinks[slicestart:])), end=' \\\n')
+print("\t--smoothing-sigmas {}mm".format("x".join(blurs[slicestart:])), end=' \\\n')
+print("\t--masks [ ${fixedmaskfine},${movingmaskfine} ]")
--- bin/mb_generate_iterations_singlestep_resscale.py
+++ bin/mb_generate_iterations_singlestep_resscale.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-#This file generates steps of affine registration between two images and attempts to compensate
-#For ANTs' dependency on the resolution of the file
+# This file generates steps of affine registration between two images and attempts to compensate
+# For ANTs' dependency on the resolution of the file
 
-#We do this by defining two scales to step over
-#blur_scale, which is the real-space steps in blurring we will do
-#shrink_scale, which is the subsampling scale that is 1/2 the fwhm blur scale, adjusted for file minimum resolution
+# We do this by defining two scales to step over
+# blur_scale, which is the real-space steps in blurring we will do
+# shrink_scale, which is the subsampling scale that is 1/2 the fwhm blur scale, adjusted for file minimum resolution
 
 from __future__ import division, print_function
 
@@ -13,20 +13,21 @@ import sys
 import numpy as np
 
 if len(sys.argv) == 2:
-  resolution = float(sys.argv[1])
+    resolution = float(sys.argv[1])
 else:
-  resolution = 1.0
+    resolution = 1.0
 
 shrinks = []
 blurs = []
 iterations = []
-fwhm_to_sigma = 2*np.sqrt(2*np.log(2))
+fwhm_to_sigma = 2 * np.sqrt(2 * np.log(2))
 
 blur_scale = 32
 start_scale = blur_scale / 2 / resolution
 
 for shrink_scale in range(int(np.ceil(start_scale)), 0, -1):
-    shrinks.append(str(int(min(8.0/resolution,max(1.0,np.around(shrink_scale))))))
+    shrinks.append(
+        str(int(min(8.0 / resolution, max(1.0, np.around(shrink_scale))))))
     blurs.append(str(shrink_scale * 2 * resolution / fwhm_to_sigma))
     iterations.append(str(min(2025, int(25 * 3**(shrink_scale)))))
 
@@ -34,6 +35,6 @@ shrinks.append("1")
 blurs.append("0")
 iterations.append("25")
 
-print("--convergence [{},1e-6,10]".format("x".join(iterations)), end=' ')
+print("--convergence [ {},1e-6,10 ]".format("x".join(iterations)), end=' ')
 print("--shrink-factors {}".format("x".join(shrinks)), end=' ')
 print("--smoothing-sigmas {}mm".format("x".join(blurs)), end=' ')
